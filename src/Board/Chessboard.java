4f6286e9b663b73525cee94a7f8bba80730b3102
klasocki <karolasocki@gmail.com>
2018-09-13 11:12:57 +0200

    Add turn mechanism, and some comments

--- src/Board/Chessboard.java
+++ src/Board/Chessboard.java
@@ -1,13 +1,15 @@
+//This is the Model for the Chessboard. Using Singleton pattern
+
 package Board;
 
 import javafx.scene.shape.Rectangle;
 
-import java.util.ArrayList;
 
 public class Chessboard {
     final int size = 8;
 
     private Chessboard() {
+        //Filling the chessboard with Rectangles
         chessboard = new Rectangle[size][size];
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
@@ -18,8 +20,17 @@ public class Chessboard {
 
     private static final Chessboard instance = new Chessboard();
     private static Rectangle[][] chessboard;
-
+    private static boolean WolfsTurn = true; //Wolf starts the game
+    
     public static Chessboard getInstance() {
         return instance;
+    }
+
+    public static boolean WolfsTurn() {
+        return WolfsTurn;
+    }
+
+    public static void changeTurn() {
+        WolfsTurn = !WolfsTurn;
     }
 }
